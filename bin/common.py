2e556854844b3196aa05944db7025797ac578864
Jean-François Roche <jfroche@affinitic.be>
2016-09-16 00:42:04 +0200

    Add caching and verify project status before returing

--- bin/common.py
+++ bin/common.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import ovh
+from utils import memoize
 
 
+@memoize
 def get_projects(project_name=None):
     client = ovh.Client()
     for project in client.get('/cloud/project'):
+        project_details = client.get('/cloud/project/{}'.format(project))
         if project_name:
-            project_details = client.get('/cloud/project/{}'.format(project))
             if project_name != project_details['description']:
                 continue
-        yield project
+        if project_details['status'] == 'ok':
+            yield project
