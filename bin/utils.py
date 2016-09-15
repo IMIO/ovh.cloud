4bdd05093a32537477cbccfb854fcb694ae9a58d
Jean-François Roche <jfroche@affinitic.be>
2016-09-16 00:42:55 +0200

    add memoize helper function

--- bin/utils.py
+++ bin/utils.py
@@ -41,6 +41,23 @@ def file_is_locked(file_path):
         return True
 
 
+def memoize(f):
+    """ Memoization decorator for functions taking one or more arguments. """
+
+    class memodict(dict):
+
+        def __init__(self, f):
+            self.f = f
+
+        def __call__(self, *args):
+            return self[args]
+
+        def __missing__(self, key):
+            ret = self[key] = self.f(*key)
+            return ret
+    return memodict(f)
+
+
 def singleton():
     if file_is_locked('/code/lock'):
         log.warn('another instance is running exiting now')
